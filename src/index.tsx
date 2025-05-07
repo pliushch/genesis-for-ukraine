3877b37facd2d19ba615e77f16809f1a889f1980
Maksym Pliushch <makcldsnetua@gmail.com>
2025-05-07 19:41:32 +0200

    feat: add project

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import 'src/assets/styles/index.scss';
 import App from 'src/components/App';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import {createRoot} from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/genesis-for-ukraine">
     <App/>
-  </HashRouter>
+  </BrowserRouter>
 );
