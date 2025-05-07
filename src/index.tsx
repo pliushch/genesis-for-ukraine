4b5df93429ee8a62064010b63e53b8c199404b90
Maksym Pliushch <makcldsnetua@gmail.com>
2025-05-07 19:20:09 +0200

    feat: add project

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import 'src/assets/styles/index.scss';
 import App from 'src/components/App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {createRoot} from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App/>
-  </BrowserRouter>
+  </HashRouter>
 );
